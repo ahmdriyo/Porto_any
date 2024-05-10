a5583d6756b72c381e92fe4cb380e83f26d80a97
ahmadriyo <ahmadriyo.tbn@gmail.com>
2024-05-10 09:12:17 +0800

    add about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,34 +1,17 @@
-import React from 'react'
-import Navbar from '../component/navbar'
-
+import React from "react";
+import Navbar from "../component/navbar";
+import About from ".";
+import Footer from "../component/footer";
 const about = () => {
   return (
     <>
-    <Navbar/>
-    <div className="bg-[#baf1ac] h-screen pb-10 flex flex-row justify-around pt-20">
-        <div className=" w-1/2"> 
-          <h2 className="mb-2 font-sans font-bold">About</h2>
-          <p>Ahmad Riyo Kusuma Front End Engineer <br />Hi, I am Farih, a Professional Software Developer.</p>
-          <br />
-          <p>I commenced my professional journey in 2020 as a Web Front End programmer, engaging in a multitude of projects and contributing to the development of diverse applications. My expertise primarily revolves around the Front End aspect, encompassing web, and mobile applications.</p>
-          <br />
-          <p>I have a strong passion for technology and science. I enjoy coding and constantly seek opportunities to expand my knowledge and skills in building innovative solutions. It is this passion that drives my determination to excel and become a proficient engineer.</p>
-        </div>
-        <div className="justify-start items-start w-96">
-          <h2 className="mb-2 font-sans font-bold">Skill</h2>
-          <div className="flex flex-wrap w-96">
-            <p className="skil-border border-[#3e6534]">#Node JS</p>
-            <p className="skil-border border-[#6bec4b]">#React Native</p>
-            <p className="skil-border border-[#7faaef]">#React JS</p>
-            <p className="skil-border border-[#78b06a]">#MongoDB</p>
-            <p className="skil-border border-[#e63737]">#RES API</p>
-            <p className="skil-border border-[#f78ccb]">#MYSQL</p>
-            <p className="skil-border border-[#882c9a]">#MERN</p>
-          </div>
-        </div>
+      <Navbar />
+      <div className="cts h-screen">
+        <About />
       </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default about
+export default about;
