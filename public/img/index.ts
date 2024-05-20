99af58d9e838e8c24002941f14bafeaf8e5eed20
ahmadriyo <ahmadriyo.tbn@gmail.com>
2024-05-20 13:05:50 +0800

    add asset img

---
+++ public/img/index.ts
@@ -0,0 +1,19 @@
+import discord from "../img/discord.svg";
+import call from "../img/call.svg";
+import fb from "../img/fb.svg";
+import location from "../img/location.svg";
+import email from "../img/email.svg";
+import github from "../img/github.svg";
+import instagram from "../img/instagram.svg";
+import linkedin from "../img/linkedin.svg";
+
+export {
+  discord,
+  call,
+  fb,
+  location,
+  email,
+  github,
+  instagram,
+  linkedin
+}
